3ae7663c06a8f244996cfe361f1b1169db03c3da
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-14 18:21:34 +0300

    Add Files

---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRouter from "./routes/authRoute.js";
+import { register } from "./controllers/auth.js";
+
+/****   CONFIGURATIONS       */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); ///
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(".assets", express.static(path.join(__dirname, "public/assets")));
+
+/** File Storage  */
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+//**   Routes WITH FILES     */
+
+app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRouter);
+
+/** Mongoose Setup  */
+
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Connected to ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not Connect`));
