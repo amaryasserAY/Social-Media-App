2ed002c9c86976a84dc443416bacb1d72b60af1e
amaryasserAY <tokyocode.ay@gmail.com>
2024-10-13 17:00:23 +0300

    Finish Form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
-export default LoginPage
+  const theme = useTheme();
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem "
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia, the Social Media for Sociopedia!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
